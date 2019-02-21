7e2187028a3d923fb6681fc1c2a8cc3e6d94cae6
Vincent Klaiber <vincentklaiber@gmail.com>
2019-02-21 11:15:21 +0100

    Added json file database

--- test/index.js
+++ test/index.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const test = require('tape');
-const autcomplete = require('../src');
+const autocomplete = require('../src');
 const provider = require('../src/provider');
 
-test('package', async t => {
-  t.equal(await autcomplete.getProvider(), provider, 'package loads provider');
+test('package', t => {
+  t.equal(autocomplete.getProvider(), provider, 'package loads provider');
   t.end();
 });
 
-test('suggestions', async (t) => {
-  const suggestions = await provider.getSuggestions({ prefix: 'bg-red-darkest' });
+test('suggestions', t => {
+  const suggestions = provider.getSuggestions({ prefix: 'bg-red-darkest' });
   t.equal(suggestions[0].text, 'bg-red-darkest', 'suggest completion bg-red-darkest');
   t.end();
 });
