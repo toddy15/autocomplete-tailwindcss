5ca73ee5e4eb04e1c2e1dfb7a0634b3cc618fbe0
Dr. Tobias Quathamer <toddy@debian.org>
2024-01-30 23:55:36 +0100

    Add tests for color preview

--- spec/provider-spec.js
+++ spec/provider-spec.js
@@ -61,7 +61,7 @@ describe("autocomplete-tailwindcss", () => {
     expect(completions[0].rightLabel).toContain(
       "--tw-bg-opacity: 1; background-color: rgb(0 0 0 / var(--tw-bg-opacity));",
     );
-    expect(completions[0].leftLabelHTML).toContain(
+    expect(completions[0].leftLabelHTML).toBe(
       '<div style="background-color: rgb(0 0 0 / 1)" class="tailwind__color-preview"></div>',
     );
   });
@@ -91,5 +91,39 @@ describe("autocomplete-tailwindcss", () => {
     editor.setCursorBufferPosition([0, 14]);
 
     expect(getCompletions()[0].text).toBe("-backdrop-hue-rotate-0");
+  });
+
+  it("does not show preview colors if there are no colors", () => {
+    provider.isTailwindProject = true;
+
+    editor.setText('<div class="justify-c');
+    editor.setCursorBufferPosition([0, 22]);
+
+    expect(getCompletions()[0].text).toBe("justify-center");
+    expect(getCompletions()[0].leftLabelHTML).toBeUndefined();
+  });
+
+  it("shows preview colors with rgb()", () => {
+    provider.isTailwindProject = true;
+
+    editor.setText('<div class="bg-sky-5');
+    editor.setCursorBufferPosition([0, 21]);
+
+    expect(getCompletions()[0].text).toBe("bg-sky-50");
+    expect(getCompletions()[0].leftLabelHTML).toBe(
+      '<div style="background-color: rgb(240 249 255 / 1)" class="tailwind__color-preview"></div>',
+    );
+  });
+
+  it("shows preview colors with hex notation", () => {
+    provider.isTailwindProject = true;
+
+    editor.setText('<div class="fill-teal-6');
+    editor.setCursorBufferPosition([0, 24]);
+
+    expect(getCompletions()[0].text).toBe("fill-teal-600");
+    expect(getCompletions()[0].leftLabelHTML).toBe(
+      '<div style="background-color: #0d9488" class="tailwind__color-preview"></div>',
+    );
   });
 });
